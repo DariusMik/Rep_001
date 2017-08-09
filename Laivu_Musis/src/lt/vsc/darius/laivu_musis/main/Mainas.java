94eaa493ec0fe24ef93928f9c56a9b7cfed53728
d.mikalajunas <d.mikalajunas@Darius.it.local>
2017-08-09 09:41:50 +0300

    atnaujinti

--- Laivu_Musis/src/lt/vsc/darius/laivu_musis/main/Mainas.java
+++ Laivu_Musis/src/lt/vsc/darius/laivu_musis/main/Mainas.java
@@ -5,8 +5,12 @@ public class Mainas {
 	public static void main(String[] args) {
 		System.out.println("testas");
 	AplinkaImplementation ap = new AplinkaImplementation();
+	ap.padetiLaiva(4, 5, 5, Kryptis.HORIZONTAL);
+	ap.padetiLaiva(3, 1, 1, Kryptis.HORIZONTAL);
 	ap.padetiLaiva(4, 1, 5, Kryptis.HORIZONTAL);
 	ap.gautiLenta();
+	System.out.println();
+	System.out.println(ap.gautiGyvuLaivuSkaiciu());
 
 	}
 
