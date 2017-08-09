97a3958ec4d08c443ea68e5f6389f9318c6fc1b6
DariusMik <darius@mikalajunas.lt>
2017-08-09 08:43:08 +0300

    Pataisyta lenta

--- Laivu_Musis/src/lt/vsc/darius/laivu_musis/main/AplinkaImplementation.java
+++ Laivu_Musis/src/lt/vsc/darius/laivu_musis/main/AplinkaImplementation.java
@@ -1,27 +1,37 @@
 package lt.vsc.darius.laivu_musis.main;
 
-import java.util.ArrayList;
 
 public class AplinkaImplementation implements Aplinka {
 	private int xMax=10;
 	private int yMax=10;
-	private ArrayList<LaivasImplementation> laivuMasyvas = new ArrayList<LaivasImplementation>();
+	private Laivas[][] lenta = new Laivas[9][9];
+	private int laivuSkaicius = 0;
 
 	@Override
 	public Laivas padetiLaiva(int dydis, int x, int y, Kryptis kryptis) {
+		x = x - 1;
+		y = y - 1;
 		LaivasImplementation naujasLaivas = null;
 		switch (kryptis) {
 		case HORIZONTAL:
-			xMax = xMax - dydis + 1;
+			xMax = xMax - dydis;
 			break;
 
 		case VERTICAL:
-			xMax = xMax - dydis + 1;
+			xMax = xMax - dydis;
 			break;
 		}
-		if (x <= xMax && x > 0 && y <= yMax && y > 0) {
+		if (x <= xMax && x >= 0 && y <= yMax && y >= 0) {
 			naujasLaivas = new LaivasImplementation(dydis, x, y, kryptis);
-			laivuMasyvas.add(naujasLaivas);
+			this.laivuSkaicius++;
+			lenta[x][y] = naujasLaivas;
+			while (dydis > 1) {
+				if (kryptis == Kryptis.VERTICAL)
+					lenta[x + dydis - 1][y] = naujasLaivas ;
+				else
+					lenta[x][y + dydis - 1] = naujasLaivas;
+				dydis--;
+			}
 
 		}
 
@@ -30,32 +40,28 @@ public class AplinkaImplementation implements Aplinka {
 
 	@Override
 	public int gautiGyvuLaivuSkaiciu() {
-		int temp = 0;
-		for (LaivasImplementation l : this.laivuMasyvas)
-			if (l.isArGyvas())
-				temp++;
-		return temp;
+		return laivuSkaicius;
 	}
 
 	@Override
 	public Laivas[][] gautiLenta() {
-		char lenta[][] = new char[11][11];
-		for (int i=1; i < 11; i++)
-			for (int j=1; j < 11; j++)
+		/*Laivas lenta[][] = new Laivas[10][10];
+		for (int i=0; i < 10; i++)
+			for (int j=0; j < 10; j++)
 				for (LaivasImplementation l : this.laivuMasyvas) {
 					if (l.gautiX() == i && l.gautiY() == j) {
-						lenta[i][j] = 'L';
+						lenta[i][j] = l;
 						int dydis = l.gautiDydi();
 						while (dydis > 1) {
 							if (l.gautiKrypti() == Kryptis.VERTICAL)
-								lenta[i + dydis - 1][j] = 'L';
+								lenta[i + dydis - 1][j] = l;
 							else
-								lenta[i][j + dydis - 1] = 'L';
+								lenta[i][j + dydis - 1] = l;
 							dydis--;
 						}
 
-					} else if (lenta[i][j] != 'L')
-						lenta[i][j] = '#';
+					} else if (lenta[i][j] != l)
+						lenta[i][j] = null;
 
 				}
 		System.out.print("   1 2 3 4 5 6 7 8 9 10");
@@ -66,8 +72,8 @@ public class AplinkaImplementation implements Aplinka {
 			for (int j=1; j < 11; j++)
 			System.out.print(" " + lenta[i][j]);
 			
-		}
-		return null;
+		}*/
+		return lenta;
 	}
 
 	@Override
@@ -78,7 +84,7 @@ public class AplinkaImplementation implements Aplinka {
 
 	@Override
 	public int gautiLaivuSkaiciu() {
-		return this.laivuMasyvas.size();
+		return this.laivuSkaicius;
 	}
 
 }
--- Laivu_Musis/src/lt/vsc/darius/laivu_musis/main/Mainas.java
+++ Laivu_Musis/src/lt/vsc/darius/laivu_musis/main/Mainas.java
@@ -3,11 +3,13 @@ package lt.vsc.darius.laivu_musis.main;
 public class Mainas {
 
 	public static void main(String[] args) {
+	Laivas [][] lenta;
 		System.out.println("testas");
 	AplinkaImplementation ap = new AplinkaImplementation();
 	ap.padetiLaiva(4, 1, 5, Kryptis.HORIZONTAL);
-	ap.gautiLenta();
-
+	ap.padetiLaiva(3, 3, 5, Kryptis.HORIZONTAL);
+	lenta = ap.gautiLenta();
+	System.out.println(lenta[0][8].gautiDydi());
 	}
 
 }
